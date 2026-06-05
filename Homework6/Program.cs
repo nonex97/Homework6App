a843a7e8a6eeff85c6f4c68b3c23a7dd447e8d4e
nonex97 <ngrguric97@gmail.com>
2026-06-05 19:27:28 +0200

    App that has an array of names, asks user for an index, checks if the input is valid and in bounds, if yes then prints the name at that index.

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,4 +1,28 @@
-﻿// 
+﻿// Create an array of 3 names. Ask the user which number to select.
+// When the user gives a number, return that name.
+// Make sure to check for invalid numbers.
 
 
+string[] firstNames = new string[] { "Neven", "Yukica", "John" };
+string userChoiceText;
+int userChoice;
+bool isValidNumber;
 
+do
+{
+    Console.Write("Enter a number to select a name: ");
+    userChoiceText = Console.ReadLine();
+    isValidNumber = int.TryParse(userChoiceText, out userChoice);
+    if (isValidNumber == false)
+    {
+        Console.WriteLine("Invalid number! Please enter a valid number.");
+    }
+    else if (userChoice > firstNames.Length - 1 || userChoice < 0)
+    {
+        Console.WriteLine("There is no name at this index!");
+    }
+    else
+    {
+        Console.WriteLine($"The name is {firstNames[userChoice]}.");
+    }
+} while (isValidNumber == false);
